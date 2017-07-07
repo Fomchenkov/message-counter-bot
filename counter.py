13a42548dad1f898c6767dba350e3e7d0c454f0d
Vyacheslav <slava290900@outlook.com>
2017-07-07 11:44:32 +0300

    Bot counts only group messages.

--- counter.py
+++ counter.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import sys
 
 import telebot
 
 
 DB_NAME = 'db.db'
-BOT_TOKEN = '418608473:AAE4NnLbjUPx9xIZp-vmUZZZOSwm-c8V4lw'
+BOT_TOKEN = '418608473:AAE4NnLbjUPx9xIZp-vmUZZZOSwm-c8V4lw' # sys.argv[1]
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
@@ -24,7 +25,9 @@ with sqlite3.connect(DB_NAME) as connection:
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    text = 'Я бот для подсчета сообщений.'
+    text = 'Я бот для подсчета сообщений, отправленных пользователями в группе.\n\n'
+    text += 'Бот разработан студией Kronver.\nКонтакты: https://t.me/fomchenkov_v \n'
+    text += 'Наш бот: https://t.me/Kronver_bot'
     return bot.send_message(message.chat.id, text)
 
 
@@ -47,8 +50,6 @@ def start_command(message):
             text += '{}. {} - {} сообщений.\n'.format(i, u[2], u[3])
             i += 1
 
-        print(text)
-
         return bot.send_message(cid, text, parse_mode='html')
 
 
@@ -57,33 +58,33 @@ def text_photo_handler(message):
     """
     Инкрементировать счетчик сообщений от данного пользователя.
     """
-    uid = message.from_user.id
-    cid = message.chat.id
+    # Принимать только сообщения из групп
+    if message.chat.type == 'group':
+        uid = message.from_user.id
+        cid = message.chat.id
 
-    with sqlite3.connect(DB_NAME) as connection:
-        cursor = connection.cursor()
-        sql = 'SELECT * FROM users WHERE uid=?'
-        cursor.execute(sql, (str(uid), ))
-        res = cursor.fetchone()
-        print(res)
-        # Если пользователь есть в базе
-        if res:
-            count = res[3] + 1
-            sql = 'UPDATE users SET count=? WHERE uid=?'
-            cursor.execute(sql, (str(count), str(uid))) 
-        else:
-            uname = '{!s}'.format(message.from_user.first_name)
-            if not message.from_user.last_name == None:
-                uname += ' {!s}'.format(message.from_user.last_name)
-            """
-            if not message.from_user.username == None:
-                uname += ' (@{!s})'.format(message.from_user.username)
-            """
-            # Добавить пользователя в базу
-            sql = 'INSERT INTO users (uid, uname, count) VALUES (?, ?, ?)'
-            cursor.execute(sql, (str(uid), str(uname), str(1)))
-        connection.commit()
-    return
+        with sqlite3.connect(DB_NAME) as connection:
+            cursor = connection.cursor()
+            sql = 'SELECT * FROM users WHERE uid=?'
+            cursor.execute(sql, (str(uid), ))
+            res = cursor.fetchone()
+            # Если пользователь есть в базе
+            if res:
+                count = res[3] + 1
+                sql = 'UPDATE users SET count=? WHERE uid=?'
+                cursor.execute(sql, (str(count), str(uid))) 
+            else:
+                uname = '{!s}'.format(message.from_user.first_name)
+                if not message.from_user.last_name == None:
+                    uname += ' {!s}'.format(message.from_user.last_name)
+                """
+                if not message.from_user.username == None:
+                    uname += ' (@{!s})'.format(message.from_user.username)
+                """
+                # Добавить пользователя в базу
+                sql = 'INSERT INTO users (uid, uname, count) VALUES (?, ?, ?)'
+                cursor.execute(sql, (str(uid), str(uname), str(1)))
+            connection.commit()
 
 
 def main():
